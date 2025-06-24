63b7ad132db6d316c10668e205ab74c67b55c44b
hashiifab <hashiifidnsolo@gmail.com>
2025-06-25 06:23:12 +0700

    add haeder

---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="bg-white flex justify-around items-center pt-6 sticky top-0">
+      <div className="flex gap-4 items-center">
+        <h1 className="text-2xl font-bold">Sumopod</h1>
+        <a className="hover:text-blue-500" href="/">
+          home
+        </a>
+        <a className="hover:text-blue-500" href="/">
+          template
+        </a>
+        <a className="hover:text-blue-500" href="/">
+          pricing
+        </a>
+        <a className="hover:text-blue-500" href="/">
+          features
+        </a>
+      </div>
+
+      <div>
+        <div className="flex gap-4">
+          <button className="bg-gray-300 text-black p-2 rounded-md">
+            Login
+          </button>
+          <button className="bg-blue-500 text-white p-2 rounded-md">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
