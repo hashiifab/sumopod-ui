ba45d1a9dd77b640c9240a0e2388892e04e831c9
hashiifab <hashiifidnsolo@gmail.com>
2025-06-27 10:32:56 +0700

    refactor(supabase.ts): refactoring supabase.ts using env

--- src/supabase.ts
+++ src/supabase.ts
@@ -3,10 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supaBaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supaBaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supaBaseUrl, supaBaseAnonKey, {
-	auth: {
-		autoRefreshToken: true,
-		persistSession: true,
-		detectSessionInUrl: true,
-	},
-});
+export const supabase = createClient(supaBaseUrl, supaBaseAnonKey);
